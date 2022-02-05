fdb98953f5ff14a70451c03411fc8053787ff646
Apllify <ralilahlou50@gmail.com>
2022-02-05 15:26:49 +0100

    got scrolling working

---
+++ FebruaryGameJam/Scripts/BanningSimulator.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var posts = [
+	"I love kissing the ass of an authoritarian government that has killed 60-80 million of its own citizens",
+	"Eastasia would like to have a censored one sided chat with you",
+	
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func generate_front_page():
+	pass
