386944a05d8fd4dcf239a4e0c7cd8e04959acce7
Apllify <ralilahlou50@gmail.com>
2022-02-05 17:13:16 +0100

    added random front page generation

--- FebruaryGameJam/Scripts/BanningSimulator.gd
+++ FebruaryGameJam/Scripts/BanningSimulator.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var post_class
+var rng 
+
+var scrollbox_reference
 
 var bad_thinks = [
 	"I love kissing the ass of an authoritarian government that has killed 60-80 million of its own citizens",
@@ -13,7 +16,7 @@ var bad_thinks = [
 	
 ]
 
-var good_think = [
+var good_thinks = [
 	"[Guide] How to shut down any anti-government talking points",
 	"How to convince people that revolutionaries and anarchists are usseless fucking parasites?",
 	"My parents just spent an hour trying to convince me that we have a dictatorship, what do ? ",
@@ -21,10 +24,48 @@ var good_think = [
 	
 ]
 
+var random_adjectives = [
+	"Ancient",
+	"Busy",
+	"Lazy",
+	"Hollow",
+	"Low",
+	"New",
+	"Safe",
+	"Wild"
+]
+
+var random_animals = [
+	"Ant",
+	"Cow",
+	"Dog",
+	"Dolphin",
+	"Fox",
+	"Frog",
+	"Impala",
+	"Scorpion",
+	"Bull"
+]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	post_class = load("res://Prefabs/Post.tscn")
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	scrollbox_reference = get_node("ScrollContainer").get_node("VBoxContainer")
+	
+	generate_front_page()
+	
+
+func generate_username():
+	var adjective_index = rng.randi_range(0, len(random_adjectives) - 1)
+	var animal_index = rng.randi_range(0, len(random_animals) - 1)
+	var number = rng.randi_range(0, 999)
+	
+	return random_adjectives[adjective_index] + random_animals[animal_index] + str(number)
+	
 
 
 func generate_front_page():
@@ -33,5 +74,25 @@ func generate_front_page():
 		#create post instance
 		var post_instance = post_class.instance()
 		
-		#get post text_field :
+		#determin if good think or bad think
+		var is_good_think = rng.randi_range(0, 1) == 1
 		
+		var random_post = ""
+		
+		if is_good_think : 
+			var random_post_index = rng.randi_range(0, len(good_thinks) - 1)
+			random_post = good_thinks[random_post_index]
+		else:
+			var random_post_index = rng.randi_range(0, len(bad_thinks) - 1)
+			random_post = bad_thinks[random_post_index]
+		
+		var random_username = generate_username()
+		
+		post_instance.is_good_think = is_good_think
+		post_instance.post_content = random_post
+		post_instance.post_author = random_username
+		
+		scrollbox_reference.add_child(post_instance)
+		
+	var final_control = Control.new()
+	scrollbox_reference.add_child(final_control)	
--- FebruaryGameJam/Scripts/Post.gd
+++ FebruaryGameJam/Scripts/Post.gd
@@ -2,18 +2,23 @@ extends Control
 
 
 var is_good_think = false
+var post_content = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+var post_author = "username"
 
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var post_content_field = get_node("PostContent")
+	post_content_field.set_text(post_content)
+	
+	var post_author_field = get_node("PostAuthor")
+	post_author_field.set_text("posted by u/" + post_author)
 
 
 
 
-#when the post is upvoted
 func _on_upvoted():
 	print("upvote !") 
 	
